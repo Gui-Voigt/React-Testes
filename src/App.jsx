2a09bb53bc1bb7831631fee645499a16548548aa
Voigt <guilherme.voigt@outlook.com>
2022-03-16 21:21:34 -0300

    fragments

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import '../index.css'
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
+import List from './components/List'
 
 function App() {
   return (
     <div>
       <HelloWorld/>
       <SayMyName name="Guilherme"/>
+      <List/>
     </div>
   )
 }
